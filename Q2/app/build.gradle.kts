ba52f5f3ae56120b1252a1c4e951b9d6157aa447
Rakshit Chhabra <252430991+rakshit1902@users.noreply.github.com>
2026-04-04 20:34:48 +0530

    Initial Audio tab layout

--- Q2/app/build.gradle.kts
+++ Q2/app/build.gradle.kts
@@ -38,7 +38,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.cardview)
+    implementation(libs.viewpager2)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.material:material:1.11.0")
 }
